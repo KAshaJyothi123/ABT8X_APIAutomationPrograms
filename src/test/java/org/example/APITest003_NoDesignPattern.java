03be1edaed46d38f40767b65571a534c5f567ebe
ashajk <asha75k@gmail.com>
2025-03-19 14:02:11 +0530

    27th project manven

---
+++ src/test/java/org/example/APITest003_NoDesignPattern.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class APITest003_NoDesignPattern
+{
+    public void Step1()
+    {
+        System.out.println("Step 1");
+    }
+    public void Step2()
+    {
+        System.out.println("Step 2");
+    }
+    public void Step3(String name)
+    {
+        System.out.println("Step 3");
+    }
+
+    public static void main(String[] args) {
+        APITest003_NoDesignPattern Api=new APITest003_NoDesignPattern();
+        Api.Step1();
+        Api.Step2();
+        Api.Step3("Asha");
+    }
+}
---
+++ src/test/java/org/example/APITest04_BuilderDesignPattern.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class APITest04_BuilderDesignPattern
+{
+    public APITest04_BuilderDesignPattern Step1()
+    {
+        System.out.println("Step1 is Started");
+        System.out.println("step1 is done");
+        return this;
+    }
+
+    public APITest04_BuilderDesignPattern Step2()
+    {
+        System.out.println("Step2 is started");
+        System.out.println("Step2 is done");
+        return this;
+    }
+    public APITest04_BuilderDesignPattern Step3(String name)
+    {
+        System.out.println("Step3 is started");
+        System.out.println("Step3 is done:" +name);
+        return this;
+    }
+
+    public static void main(String[] args) {
+        APITest04_BuilderDesignPattern api=new APITest04_BuilderDesignPattern();
+        api.Step1().Step2().Step3("Asha");
+    }
+}
