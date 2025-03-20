30194699e44998740621de577da8a34c0ba2658b
ashajk <asha75k@gmail.com>
2025-03-20 15:20:52 +0530

    20th integration Testing

---
+++ src/test/java/org/example/RestAssuredBasic/GET/Intergaration/APITesting_011_TestCaseIntegration.java
@@ -0,0 +1,14 @@
+package org.example.RestAssuredBasic.GET.Intergaration;
+
+public class APITesting_011_TestCaseIntegration
+{
+
+    //Create a Tokken
+    //Create a Booking
+    //perform a Put
+    //Verify that PUT is sucess by GET Request
+    //Delete the ID
+    //verify if doesn't exist GET Request
+
+
+}
