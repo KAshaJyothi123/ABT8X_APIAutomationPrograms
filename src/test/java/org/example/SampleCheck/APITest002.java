3ede7732cb107d0ef7e2a2cd50f99da087809f15
ashajk <asha75k@gmail.com>
2025-03-20 12:19:58 +0530

    27th project manven

--- src/test/java/org/example/SampleCheck/APITest002.java
+++ src/test/java/org/example/SampleCheck/APITest002.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.SampleCheck;
 
 import io.restassured.RestAssured;
 
@@ -6,8 +6,14 @@ public class APITest002
 {
     public static void main(String[] args) {
 
+        //Full url:" https://restful-booker.herokuapp.com/booking/1"
+
+        //Base uri:" https://restful-booker.herokuapp.com/"
+        //Base path:" booking/1"
+
+
         RestAssured
-                .given()
+                .given().relaxedHTTPSValidation()
                     .baseUri("https://restful-booker.herokuapp.com/")
                     .basePath("booking/1")
                 .when().
--- src/test/java/org/example/SampleCheck/APITest04_BuilderDesignPattern.java
+++ src/test/java/org/example/SampleCheck/APITest04_BuilderDesignPattern.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.SampleCheck;
 
 public class APITest04_BuilderDesignPattern
 {
